5c2a53e013ead9acf8e0e8db169cf7f3d66a872d
Niklas Buhl <hello@niklasbuhl.com>
2023-06-28 20:57:36 +0200

    test

---
+++ learning/setPixel.py
@@ -0,0 +1,26 @@
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+import random
+import time
+
+options = RGBMatrixOptions()
+options.rows = 32
+options.chain_length = 1
+options.parallel = 1
+options.disable_hardware_pulsing = True
+options.hardware_mapping = adafruit-hat
+options.gpio_slowdown = 1
+
+matrix = RGBMatrix(options = options)
+frame_canvas = matrix.CreateFrameCanvas()
+
+start = time.time()
+
+for x in Range(32):
+	for y in Range(32):
+		frame_canvas.SetPixel(x,y,random.randint(0, 255), random.randint(0, 255), random.randint(0, 9))
+
+matrix.SwapOnVSync(frame_canvas)
+
+end = time.time()
+
+print(f"run: ${end - start}")
